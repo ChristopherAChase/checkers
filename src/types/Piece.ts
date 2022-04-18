9455465e7aa5cda653267c79b7015bb2c670c73f
Christopher Chase <Christopher.A.Chase@outlook.com>
2022-04-18 07:49:25 -0500

    removed implementation logic out of piece types file, and extracted coordinates information out

--- src/types/Piece.ts
+++ src/types/Piece.ts
@@ -1,34 +1,15 @@
+import { Coordinate, Cell } from './Board';
 
 type Color = 'Red' | 'Black';
 
-type OddColumns = 'a' | 'c' | 'e' | 'g';
-type EvenColumns = 'b' | 'd' | 'f' | 'h';
+type Rank = 'Man' | 'King';
 
-export type Coordinate = `${OddColumns}${1 | 3 | 5 | 7}` | `${EvenColumns}${2 | 4 | 6 | 8}`
-
-// My attempt at saying that if this is a piece of rank 'king', then the rank has to be 'king',
-// and if it's a piece of rank 'man', then the rank has to be 'man'.
-export type Piece<Rank = 'Man' | 'King'> = {
+export type Piece = {
     rank: Rank,
     color: Color
     coordinate: Coordinate
 };
 
+export type getValidMoves = (cells: Cell[], piece: Piece) => Cell[];
 
-const isValidRow = (row: string): boolean => parseInt(row) >= 1 && parseInt(row) <= 8;
-const isValidColumn = (column: string) => ['a', 'b', 'c','d', 'e', 'f' ,'g', 'h'].includes(column);
-
-// By passing in a coordinate instead of a piece, this can be ran recursively in the event of jumping over an opponent.
-export const getSurroundingCells = (coordinate: Coordinate): Coordinate[] => {
-    const [column , row] = coordinate;
-    const surroundingRows = [`${parseInt(row) - 1}`, `${parseInt(row) + 1}`].filter(isValidRow);
-    const surroundingColumns = [String.fromCharCode(column.charCodeAt(0) - 1), String.fromCharCode(column.charCodeAt(0) + 1)].filter(isValidColumn);
-    const surroundingCells: Coordinate[] = [];
-
-    for(let col in surroundingColumns){
-        for(let row in surroundingRows){
-            surroundingCells.push(`${surroundingColumns[col]}${surroundingRows[row]}` as Coordinate)
-        }
-    }
-    return surroundingCells
-};
+export type jumpPiece = (piece: Piece, newDestination: Cell) => Piece;
